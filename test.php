f62dfc70fe462f5c2d39ec5c3b8c2525869e4b1a
BTtea <32271123+BTtea@users.noreply.github.com>
2024-04-28 18:01:27 +0800

    hidden xss test

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+function filter_xss($payload){
+    $payload=str_replace(">","",$payload);
+    $payload=str_replace("<","",$payload);
+    $payload=str_replace("'","",$payload);
+    $payload=str_replace("(","",$payload);
+    $payload=str_replace(")","",$payload);
+    return $payload;
+}
+
+if(isset($_GET['xss'])){
+    $r=filter_xss($_GET['xss']);
+    echo "<input type=\"hidden\" value=\"$r\">";
+}
+else{
+    highlight_file(__FILE__);
+}
+?>
